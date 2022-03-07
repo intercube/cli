c003f5a3fb564ae1801f4f135bca1a94688f6d3f
Jeroen Ketelaar <jeroen@ketelaar.me>
2022-03-07 21:03:53 +0100

    Updated to 2022

--- cmd/install.go
+++ cmd/install.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2021 Intercube <opensource@intercube.io>
+Copyright © 2022 Intercube <opensource@intercube.io>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
